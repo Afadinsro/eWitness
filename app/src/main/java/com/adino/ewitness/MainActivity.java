84d461596cea947e3bdcf0bdf46e0797c02010a5
Afadinsro <mills.josef@gmail.com>
2017-12-06 10:07:20 +0000

    Successfully implemented timer for image slider

--- app/src/main/java/com/adino/ewitness/MainActivity.java
+++ app/src/main/java/com/adino/ewitness/MainActivity.java
@@ -2,17 +2,20 @@ package com.adino.ewitness;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.ViewPager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -30,6 +33,9 @@ public class MainActivity extends AppCompatActivity
         viewPager = (ViewPager) findViewById(R.id.vp_image_slider);
         viewPagerAdapter = new ViewPagerAdapter(this);
         viewPager.setAdapter(viewPagerAdapter);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new ImageSliderTimer(), 2000, 6000);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -100,5 +106,26 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    /**
+     * Implementing Timer for image slider
+     */
+    public class ImageSliderTimer extends TimerTask{
+
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(viewPager.getCurrentItem() == 0){
+                        viewPager.setCurrentItem(1, true);
+                    } else if(viewPager.getCurrentItem() == 1){
+                        viewPager.setCurrentItem(0, true);
+                    }
+                }
+            });
+        }
     }
 }
