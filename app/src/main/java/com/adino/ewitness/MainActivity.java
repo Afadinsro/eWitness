16861073dae4b414c3bebb2bd3cc1d57871df24c
Afadinsro <mills.josef@gmail.com>
2017-12-06 13:30:10 +0000

    Successfully able to take pictures using camera intent

--- app/src/main/java/com/adino/ewitness/MainActivity.java
+++ app/src/main/java/com/adino/ewitness/MainActivity.java
@@ -1,15 +1,21 @@
 package com.adino.ewitness;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,10 +24,12 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, ActivityCompat.OnRequestPermissionsResultCallback{
 
     private ViewPager viewPager;
     private ViewPagerAdapter viewPagerAdapter;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private  static final String TAG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +49,12 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Intent cameraIntent = new Intent();
+                cameraIntent.setClass(MainActivity.this, CameraActivity.class);
+                startActivity(cameraIntent);*/
+                if(isPermissionGrantedForCamera()) {
+                    dispatchCameraIntent();
+                }
             }
         });
 
@@ -108,7 +120,42 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+            //resume tasks needing this permission
+        }
+    }
 
+    /*******************************PERSONAL METHODS****************************************/
+
+    private void dispatchCameraIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public  boolean isPermissionGrantedForCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+                return false;
+            }
+        } else {
+            //permission is automatically granted on sdk > 23
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
+    }
+
+    /******************************IMAGE SLIDER TIMER*************************************/
     /**
      * Implementing Timer for image slider
      */
